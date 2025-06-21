ef3c5640a80db8419a605449d22ade0279af2812
Fred Simmons <fsimmons@gmail.com>
2025-06-21 14:32:30 -0400

    Update posts.php

--- patterns/posts.php
+++ patterns/posts.php
@@ -14,14 +14,8 @@
 <!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"className":"entry-header"} -->
 <header class="wp-block-group entry-header">
 <!-- wp:post-title {"isLink":true} /-->
-<!-- wp:group {"style":{"spacing":{"blockGap":"5px","margin":{"bottom":"30px"}},"typography":{"fontSize":"18px"}},"className":"post-meta","layout":{"type":"flex"}} -->
-<div class="wp-block-group post-meta" style="margin-bottom:30px;font-size:18px">
-<!-- wp:post-date /-->
-</div>
-<!-- /wp:group -->
 </header>
 <!-- /wp:group -->
-<!-- wp:post-excerpt {"moreText":"Read More"} /-->
 </article>
 <!-- /wp:group -->
 <!-- /wp:post-template -->
